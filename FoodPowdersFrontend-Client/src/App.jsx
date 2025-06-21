9a386ee0baeb84dec24ba672697626492d8baf1c
Akilan13kr <akilan13kr@gmail.com>
2025-06-21 19:47:50 +0530

    update in exploremenu, menubar and added it in app.jsx

--- FoodPowdersFrontend-Client/src/App.jsx
+++ FoodPowdersFrontend-Client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Menubar from './Components/Menubar/Menubar';
+import {Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Contact from './Pages/Contact/Contact';
+import ExploreFoodPowder from './Pages/ExploreFoodPowder/ExploreFoodPowder';
+
+
 
 const App = () => {
   return (
     <div>
       <Menubar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/explore' element={<ExploreFoodPowder />}/>
+      </Routes>
     </div>
   )
 }
--- FoodPowdersFrontend-Client/src/Components/Menubar/Menubar.jsx
+++ FoodPowdersFrontend-Client/src/Components/Menubar/Menubar.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import './Menubar.css';
 import {assets} from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
 const Menubar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container">
-    <img src={assets.logo} alt='' className='logo' height={48} width={48} />
+    <img src={assets.logo} alt='' className='mx-2' height={48} width={48} />
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link" href="#">Home</a>
+          <Link className="nav-link" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Explore</a>
+          <Link className="nav-link" to="/explore">Explore FoodPowder</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact Us</a>
+          <Link className="nav-link" to="/contact">Contact Us</Link>
         </li>
       </ul>
-      <div className="menubar-right">
+      <div className="d-flex align-item-center gap-4">
         <div className="position-relative">
             <img src={assets.cart} alt="" height={32} width={32} className='position-relative'/>
             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">5</span>
