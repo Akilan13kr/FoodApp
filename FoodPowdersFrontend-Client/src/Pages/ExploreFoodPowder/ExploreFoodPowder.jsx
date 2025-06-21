9436cdbaf30d2013b791e9e445890872719d609d
Akilan13kr <akilan13kr@gmail.com>
2025-06-21 19:37:02 +0530

    Menu, Explore, contact thing is started and added

---
+++ FoodPowdersFrontend-Client/src/Pages/ExploreFoodPowder/ExploreFoodPowder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExploreFoodPowder = () => {
+  return (
+    <div>ExploreFoodPowder</div>
+  )
+}
+
+export default ExploreFoodPowder
---
+++ FoodPowdersFrontend-Client/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../Components/Header/Header';
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu';
+
+const Home = () => {
+  return (
+    <main className='container'>
+      <Header />
+      <ExploreMenu />
+    </main>
+  )
+}
+
+export default Home;
