3846a1de05b66646d0a046f20e0d9d023f90144b
K. R. Akilan. <131351948+Akilan13kr@users.noreply.github.com>
2025-07-28 20:40:34 +0530

    Update OrderService.js with actual backend link

--- FoodPowdersFrontend-Client/src/Service/OrderService.js
+++ FoodPowdersFrontend-Client/src/Service/OrderService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api/orders';
+const API_URL = "https://foodapp-e3qb.onrender.com/api/orders";
 
 export const fetchOrdersOfUser = async (token) =>{
     try {
