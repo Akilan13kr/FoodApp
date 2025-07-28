03800a812b2d39aef7e65c98570b63dd1cd6c391
K. R. Akilan. <131351948+Akilan13kr@users.noreply.github.com>
2025-07-28 20:41:01 +0530

    Update foodService.js with actaul backend link

--- FoodPowdersFrontend-Client/src/Service/foodService.js
+++ FoodPowdersFrontend-Client/src/Service/foodService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/foodpowders";
+const API_URL = "https://foodapp-e3qb.onrender.com/api/foodpowders";
 
 export const fetchFoodPowderList = async() => {
 
