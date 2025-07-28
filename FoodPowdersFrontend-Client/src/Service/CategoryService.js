133405f63e3b8341923420118c82e528230a2e57
K. R. Akilan. <131351948+Akilan13kr@users.noreply.github.com>
2025-07-28 20:39:24 +0530

    Update CategoryService.js with actual backend

--- FoodPowdersFrontend-Client/src/Service/CategoryService.js
+++ FoodPowdersFrontend-Client/src/Service/CategoryService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/category";
+const API_URL = "https://foodapp-e3qb.onrender.com/category";
 
 export const fetchCategoryList = async() => {
 
