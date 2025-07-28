0eae96e36e797387d1e6670f8f95550154abdea5
K. R. Akilan. <131351948+Akilan13kr@users.noreply.github.com>
2025-07-28 20:39:55 +0530

    Update ContactService.js with actual backend link

--- FoodPowdersFrontend-Client/src/Service/ContactService.js
+++ FoodPowdersFrontend-Client/src/Service/ContactService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api/contact';
+const API_URL = 'https://foodapp-e3qb.onrender.com/api/contact';
 
 export const sentDetail = async(emailData) =>{
     try {
